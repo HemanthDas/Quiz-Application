5412605e4716c0943ea52bc5597708608ace37b4
HemanthDas <hemanthchoudary4@gmail.com>
2024-01-16 21:38:40 +0530

    Update:Topic Start

--- src/pages/exam.tsx
+++ src/pages/exam.tsx
@@ -3,20 +3,25 @@ import { useEffect, useState, useContext } from "react";
 import { NavbarContext } from "../context/navbarcontext";
 // import PreTest from "../components/exam.pretext";
 import ExTest from "../components/exam.extest";
+import { AuthContext } from "../context/authcontext";
 
 const Exam = () => {
   const route = useParams({ from: undefined, strict: true });
+  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
   const { hide, nohide } = useContext(NavbarContext);
   const [isFullScreen, setIsFullScreen] = useState(false);
   useEffect(() => {
+    if (!user.user) {
+      navigate({ to: "/", replace: true });
+    }
     if (isFullScreen) return;
     const enableFullscreen = async () => {
       try {
         await document.body.requestFullscreen();
         setIsFullScreen(true);
       } catch (error) {
-        nohide();
+        hide();
         setIsFullScreen(false);
       }
     };
